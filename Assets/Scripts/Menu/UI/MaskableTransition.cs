f0f39573fbe3e274e4b087dfccb881783c587878
neosInPocket <88581498+neosInPocket@users.noreply.github.com>
2023-12-08 15:36:08 +0300

    changes

---
+++ Assets/Scripts/Menu/UI/MaskableTransition.cs
@@ -0,0 +1,34 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MaskableTransition : MaskableGraphic
+{
+	[SerializeField] private float transitionSpeed;
+
+	protected override void OnPopulateMesh(VertexHelper vh)
+	{
+		vh.Clear();
+
+		Vector2 a = new Vector2(0, 0);
+		Vector2 b = new Vector2(0, 50);
+		Vector2 c = new Vector2(100 + Time.realtimeSinceStartup * transitionSpeed, 0);
+		Vector2 d = new Vector2(100 + Time.realtimeSinceStartup * transitionSpeed, 100);
+
+		var color = Color.white;
+
+		vh.AddUIVertexQuad(
+			new UIVertex[]
+			{
+				new UIVertex { position = a, color = color },
+				new UIVertex { position = b, color = color },
+				new UIVertex { position = c, color = color },
+				new UIVertex { position = d, color = color }
+			}
+		);
+	}
+
+	private void Update()
+	{
+		SetVerticesDirty();
+	}
+}
